88aa32e644a5694deff0ec6173a2919a9e8c466b
Vishal Bhardwaj <anshu41003@gmail.com>
2025-02-16 12:10:23 +0530

    [Vishal] Add: New Files added.

---
+++ PeakElement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static int FindPeakElement(int[] nums)
+    {
+        int left = 0, right = nums.Length - 1;
+        
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] > nums[mid + 1])
+                right = mid;
+            else
+                left = mid + 1;
+        }
+        
+        return left;
+    }
+
+    static void Main()
+    {
+        int[] nums = {1, 2, 3, 1};
+        int peakIndex = FindPeakElement(nums);
+        Console.WriteLine("Peak element index: " + peakIndex);
+        Console.WriteLine("Peak element value: " + nums[peakIndex]);
+    }
+}
